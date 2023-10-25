08e98fef0dd17484840733c24e50f66cec184a5b
repo-copier <info@gruntwork.io>
2023-10-24 18:55:20 -0600

    Resolve references

--- eks/tls.go
+++ eks/tls.go
@@ -4,7 +4,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
-	"github.com/gruntwork-io/gruntwork-cli/errors"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/errors"
 	"net/http"
 )
 
